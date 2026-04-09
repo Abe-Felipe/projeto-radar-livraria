64760c0be52ce421eb06795ee06112870ac41a1b
Felipe Abe <abe.fydr@gmail.com>
2026-04-09 19:27:21 -0300

    implementacao da etapa de conexao com o site

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://books.toscrape.com/"
+
+resposta = requests.get(url)
+
+# validacao da resposta
+if resposta.status_code != 200:
+    print(f"Erro ao acessar o site. Status code: {resposta.status_code}")
+    raise SystemExit()
+
+print("Conexão bem-sucedida!")
